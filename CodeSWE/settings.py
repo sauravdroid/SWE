889d2c1a9b5ff16421582d96cb1ac832fe1a7592
Saurav Biswas <sauravbiswas@Sauravs-MacBook-Pro.local>
2016-07-30 18:31:45 +0530

    Second Commit

--- CodeSWE/settings.py
+++ CodeSWE/settings.py
@@ -80,12 +80,6 @@ WSGI_APPLICATION = 'CodeSWE.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
